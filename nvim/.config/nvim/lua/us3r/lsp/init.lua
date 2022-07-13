3ea452a77d44a4836ee4d9d31456075f7d69341a
us3r <us3r@mail>
2022-07-14 00:36:40 +0200

    lsp config

--- nvim/.config/nvim/lua/us3r/lsp/init.lua
+++ nvim/.config/nvim/lua/us3r/lsp/init.lua
@@ -1,43 +1,9 @@
-local status_ok, lspconfig = pcall(require, "lspconfig")
+local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
   vim.notify("Error while loading lspconfig")
   return
 end
 
-require "configs.lsp.handlers"
-
-local signs = {
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
-}
-for _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
-vim.diagnostic.config({
-  virtual_text = true,
-  signs = { active = signs },
-  update_in_insert = true,
-  underline = true,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    source = "always",
-    header = "",
-    prefix = "",
-  },
-})
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-
-local servers = require "us3r.lsp.servers"
-local installer_avail, lsp_installer = pcall(require, "nvim-lsp-installer")
-if installer_avail then
-  for _, server in ipairs(lsp_installer.get_installed_servers()) do
-    table.insert(servers, server.name)
-  end
-end
+require "us3r.lsp.configs"
+require "us3r.lsp.handlers"
+require "us3r.lsp.null-ls"
