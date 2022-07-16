723811f6d527ba959cb56f0281fc1889ecc95d08
us3r <us3r@mail>
2022-07-16 21:16:05 +0200

    colorscheme

--- nvim/.config/nvim/lua/us3r/colorscheme.lua
+++ nvim/.config/nvim/lua/us3r/colorscheme.lua
@@ -1,5 +1,11 @@
 local colorscheme = 'catppuccin'
 
+local ok, catppuccin = pcall(require, colorscheme)
+if not ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
+
 local ok, _ = pcall(vim.cmd("colorscheme " .. colorscheme))
 if not ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
