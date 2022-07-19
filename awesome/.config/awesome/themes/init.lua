5526627b523f8c15ecd8e2c9ee944d67cfdf3595
us3r <us3r@mail>
2022-07-19 16:06:53 +0200

    still seperating the blues for you

--- awesome/.config/awesome/themes/init.lua
+++ awesome/.config/awesome/themes/init.lua
@@ -1,5 +1,7 @@
 -- Theme handling library
 local beautiful = require("beautiful")
+local awful = require("awful")
+local wibox = require("wibox")
 
 local chosen_theme = 'default'
 -- {{{ Variable definitions
@@ -7,3 +9,24 @@ local chosen_theme = 'default'
 -- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 -- beautiful.init(os.getenv('HOME') .. ".config/awsesome/themes/default/theme.lua")
 beautiful.init(string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), chosen_theme))
+
+
+-- {{{ Wallpaper
+screen.connect_signal("request::wallpaper", function(s)
+    awful.wallpaper {
+        screen = s,
+        widget = {
+            {
+                image     = beautiful.wallpaper,
+                upscale   = true,
+                downscale = true,
+                widget    = wibox.widget.imagebox,
+            },
+            valign = "center",
+            halign = "center",
+            tiled  = false,
+            widget = wibox.container.tile,
+        }
+    }
+end)
+-- }}}
