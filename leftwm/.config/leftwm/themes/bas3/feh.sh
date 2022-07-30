aa86dad8b612a95cfa3e43732624c6fbd53f3ecb
us3r <us3r@mail>
2022-07-30 08:21:27 +0200

    polybar bg

--- leftwm/.config/leftwm/themes/bas3/feh.sh
+++ leftwm/.config/leftwm/themes/bas3/feh.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-feh --no-fehbg --bg-scale "$HOME/wallpapers/drawings/majoras-mask-minimal-5k-9z-1920x1080.jpg"
+#feh --no-fehbg --bg-scale "$HOME/wallpapers/drawings/majoras-mask-minimal-5k-9z-1920x1080.jpg"
+feh --no-fehbg --bg-fill --no-xinerama '$HOME/.cache/betterlockscreen/current/wall_resize.png' 
