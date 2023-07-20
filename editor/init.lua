2162cb03342b9055d90678879c6fbe206db2cb29
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2023-07-20 22:06:31 +0200

    Lazy package manager

--- editor/init.lua
+++ editor/init.lua
@@ -1,27 +1,29 @@
--- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-  vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
 end
+vim.opt.rtp:prepend(lazypath)
 
-require('packer').startup(function(use)
-  -- Package manager
-  use 'wbthomason/packer.nvim'
+require('lazy').setup({
 
-  use {
+{
     'mrjones2014/legendary.nvim',
     version = 'v2.1.0',
     -- sqlite is only needed if you want to use frecency sorting
-    -- requires = { 'kkharji/sqlite.lua' }
-  }
+    -- dependencies = { 'kkharji/sqlite.lua' }
+  },
 
 
-  use { -- LSP Configuration & Plugins
+{ -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
-    requires = {
+    dependencies = {
       -- Automatically install LSPs to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
@@ -32,104 +34,75 @@ require('packer').startup(function(use)
       -- Additional lua configuration, makes nvim stuff amazing
       'folke/neodev.nvim',
     },
-  }
+  },
 
-  use { -- Autocompletion
+{ -- Autocompletion
     'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  }
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  },
 
-  use { -- Highlight, edit, and navigate code
+{ -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    run = function()
+    build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
-  }
+  },
 
-  use { -- Additional text objects via treesitter
+{ -- Additional text objects via treesitter
     'nvim-treesitter/nvim-treesitter-textobjects',
     after = 'nvim-treesitter',
-  }
+  },
 
   -- Git related plugins
-  use { 'NeogitOrg/neogit', requires = 'nvim-lua/plenary.nvim' }
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb'
-  use 'lewis6991/gitsigns.nvim'
+{ 'NeogitOrg/neogit', dependencies = 'nvim-lua/plenary.nvim' },
+'tpope/vim-fugitive',
+'tpope/vim-rhubarb',
+'lewis6991/gitsigns.nvim',
 
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
-  use { "LunarVim/darkplus.nvim", }
-  use 'folke/tokyonight.nvim'
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+'navarasu/onedark.nvim', -- Theme inspired by Atom
+"LunarVim/darkplus.nvim",
+{ 'folke/tokyonight.nvim', lazy=false },
+'nvim-lualine/lualine.nvim', -- Fancier statusline
+'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
+'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- Smarter Splits
-  use { "mrjones2014/smart-splits.nvim" }
+"mrjones2014/smart-splits.nvim",
 
   -- Icons
-  use { "kyazdani42/nvim-web-devicons", }
+"kyazdani42/nvim-web-devicons",
 
-  -- Bufferline
-  -- use { "akinsho/bufferline.nvim" }
+-- Bufferline
+--"akinsho/bufferline.nvim"
 
   -- Better buffer closing
-  use { "famiu/bufdelete.nvim" }
+"famiu/bufdelete.nvim",
 
   -- Notification Enhancer
-  use { "rcarriga/nvim-notify" }
+"rcarriga/nvim-notify",
 
   -- Keymaps popup
-  use { "folke/which-key.nvim" }
+"folke/which-key.nvim",
 
-  use { 'mfussenegger/nvim-jdtls' }
+'mfussenegger/nvim-jdtls',
 
   -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+{ 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
-  use { "nvim-telescope/telescope-dap.nvim" }
-  use { "nvim-telescope/telescope-packer.nvim" }
-  use { "nvim-telescope/telescope-project.nvim" }
-  use { 'nvim-telescope/telescope-ui-select.nvim' }
-  use { "cljoly/telescope-repo.nvim" }
-  use { "jvgrootveld/telescope-zoxide" }
-  use { "francoiscabrol/ranger.vim", requires = { "rbgrouleff/bclose.vim" } }
+{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+"nvim-telescope/telescope-file-browser.nvim",
+"nvim-telescope/telescope-dap.nvim",
+"nvim-telescope/telescope-packer.nvim",
+"nvim-telescope/telescope-project.nvim",
+'nvim-telescope/telescope-ui-select.nvim',
+"cljoly/telescope-repo.nvim",
+"jvgrootveld/telescope-zoxide",
+{ "francoiscabrol/ranger.vim", requires = { "rbgrouleff/bclose.vim" } },
 
-  use { "akinsho/toggleterm.nvim" }
-  -- -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-  -- local has_plugins, plugins = pcall(require, 'custom.plugins')
-  -- if has_plugins then
-  --   plugins(use)
-  -- end
+"akinsho/toggleterm.nvim",
 
-  if is_bootstrap then
-    require('packer').sync()
-  end
-end)
-
--- When we are bootstrapping a configuration, it doesn't
--- make sense to execute the rest of the init.lua.
---
--- You'll need to restart nvim, and then it will work.
-if is_bootstrap then
-  print '=================================='
-  print '    Plugins are being installed'
-  print '    Wait until Packer completes,'
-  print '       then restart nvim'
-  print '=================================='
-  return
-end
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | silent! LspStop | silent! LspStart | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
+}, {})
 
 -- Configure Plugins
 require("custom.options")
