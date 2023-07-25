1e0e8d04d726173f9b40596b68e562e6bb07b359
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2023-07-25 23:18:40 +0200

    chngs...

--- editor/init.lua
+++ editor/init.lua
@@ -63,27 +63,23 @@ require('lazy').setup({
   },
 
   -- Dap Configuration
-  { 'mfussenegger/nvim-dap', dependencies= { 'rcarriga/nvim-dap-ui', 'theHamsta/nvim-dap-virtual-text' } },
+  { 'mfussenegger/nvim-dap', dependencies = { 'rcarriga/nvim-dap-ui', 'theHamsta/nvim-dap-virtual-text' } },
 
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
-      'rafamadriz/friendly-snippets' },
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'rafamadriz/friendly-snippets' },
   },
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    build = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
+    build = ":TSUpdate",
+    dependencies = {
+      -- Additional text objects via treesitter
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'nvim-treesitter/playground',
+    }
   },
 
-  { -- Additional text objects via treesitter
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
-  },
-
-  { 'nvim-treesitter/playground', after = 'nvim-treesitter', },
 
   'nvim-lualine/lualine.nvim', -- Fancier statusline
   'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
@@ -91,11 +87,6 @@ require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   'anuvyklack/hydra.nvim',
-  -- Smarter Splits
-  -- "mrjones2014/smart-splits.nvim",
-
-  -- Better buffer closing
-  -- "famiu/bufdelete.nvim",
 
   -- Notification Enhancer
   "rcarriga/nvim-notify",
@@ -105,14 +96,14 @@ require('lazy').setup({
 
   -- Fuzzy Finder (files, lsp, etc)
   { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-      { 'nvim-telescope/telescope-live-grep-args.nvim' },
-      'nvim-telescope/telescope-file-browser.nvim',
-      'nvim-telescope/telescope-dap.nvim',
-      'nvim-telescope/telescope-project.nvim',
-      'nvim-telescope/telescope-ui-select.nvim',
-    }
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-live-grep-args.nvim',
+    'nvim-telescope/telescope-file-browser.nvim',
+    'nvim-telescope/telescope-dap.nvim',
+    'nvim-telescope/telescope-project.nvim',
+    'nvim-telescope/telescope-ui-select.nvim',
+  }
   },
 
   { "francoiscabrol/ranger.vim", lazy = false, dependencies = { "rbgrouleff/bclose.vim" },
