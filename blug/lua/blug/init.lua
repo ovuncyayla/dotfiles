fa21990650ab940dc4c869cf55f58dc1da546e57
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2023-07-28 08:36:30 +0200

    chg01

--- blug/lua/blug/init.lua
+++ blug/lua/blug/init.lua
@@ -2,10 +2,9 @@ M = {}
 local scratchpad_buf_id = -1
 
 vim.api.nvim_create_user_command("BLugEnable", function(args)
-  if vim.fn.bufexists('BLug Scratchpad') == 0 then
-    vim.notify("Creating scratch pad " .. scratchpad_buf_id)
+  if vim.fn.bufexists('BLug Scratchpad') <= 0 then
     scratchpad_buf_id = M.create_scratch_pad()
-    vim.notify("Creating scratch pad " .. scratchpad_buf_id)
+    -- vim.notify("Creating scratch pad " .. scratchpad_buf_id)
     return
   end
   vim.notify("Deleting Scratchpad")
@@ -29,12 +28,25 @@ end,
   })
 
 
+local function set_buffer_opts()
+     vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+       pattern = {"*Blug Scratchpad*"},
+       callback = function(ev)
+         vim.notify(string.format('event fired: s', vim.inspect(ev)))
+          vim.api.nvim_buf_set_option(0, 'filetype', 'lua')
+          vim.api.nvim_buf_set_option(0, 'buftype', 'acwrite')
+          vim.api.nvim_buf_call(0, function () vim.cmd("LspStart") end)
+       end
+     })
+end
+
+
 function M.create_scratch_pad()
   scratchpad_buf_id = vim.api.nvim_create_buf(true, true)
-  vim.api.nvim_buf_set_option(scratchpad_buf_id, 'filetype', 'lua')
-  vim.api.nvim_buf_set_option(scratchpad_buf_id, 'buftype', 'acwrite')
   vim.api.nvim_buf_set_name(scratchpad_buf_id, 'BLug Scratchpad')
-  vim.api.nvim_buf_call(scratchpad_buf_id, function () vim.cmd("LspStart") end)
+  -- vim.api.nvim_buf_set_option(scratchpad_buf_id, 'filetype', 'lua')
+  -- vim.api.nvim_buf_set_option(scratchpad_buf_id, 'buftype', 'acwrite')
+  -- vim.api.nvim_buf_call(scratchpad_buf_id, function () vim.cmd("LspStart") end)
 
   local width = vim.o.columns * 0.85
   local height = vim.o.lines * 0.8
@@ -54,3 +66,6 @@ function M.create_scratch_pad()
 
   return scratchpad_buf_id
 end
+
+
+vim.keymap.set("n", "<F8>", "<cmd>BLugEnable<cr>")
