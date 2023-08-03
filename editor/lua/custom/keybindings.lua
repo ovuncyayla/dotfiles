3d672e24c987d2b844aaf0fe6cdcf33d68f30518
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2023-08-03 19:06:16 +0200

    otherchgs

--- editor/lua/custom/keybindings.lua
+++ editor/lua/custom/keybindings.lua
@@ -27,10 +27,10 @@ map("n", "<leader>bb", ":Telescope file_browser path=%:p:h select_buffer=true<CR
 map('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 map('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 map('n', '<leader>/', function() require('telescope.builtin').current_buffer_fuzzy_find() end,
-  { desc = '[/] Fuzzily search in current buffer]' })
+	{ desc = '[/] Fuzzily search in current buffer]' })
 map('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 map('n', '<leader>FF', function() require('telescope.builtin').find_files({ hidden = true }) end,
-  { desc = '[S]earch [F]iles +Hidden' })
+	{ desc = '[S]earch [F]iles +Hidden' })
 map('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 map('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 -- map('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
@@ -38,12 +38,14 @@ map('n', '<leader>sg', require('telescope').extensions.live_grep_args.live_grep_
 map('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 map('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, { desc = 'Search [D]ocument [S]ymbols' })
 map("n", "<leader>sn", function() require("telescope").extensions.notify.notify() end,
-  { desc = "[S]earch [n]otifications" })
+	{ desc = "[S]earch [n]otifications" })
 map("n", "<leader>sc", function() require("telescope.builtin").commands() end, { desc = "[S]earch [c]ommands" })
 map("n", "<leader>sr", function() require("telescope.builtin").registers() end, { desc = "[S]earch [r]egisters" })
 map("n", "<leader>sm", function() require("telescope.builtin").marks() end, { desc = "[S]earch [m]arks" })
+map('n', '<leader>sp', function() require('telescope.builtin').find_files({ cwd = vim.fn.stdpath("data") }) end,
+	{ desc = '[S]earch [P]ugins' })
 map("n", "<leader>pp", function() require("telescope").extensions.project.project() end,
-  { desc = "[P]rojectile find [P]roject" })
+	{ desc = "[P]rojectile find [P]roject" })
 
 -- Lsp
 map("n", "<leader>lt", "<cmd>TroubleToggle<cr>", { desc = "Trouble Toggle" })
@@ -52,48 +54,48 @@ map("n", "<leader>ln", "<cmd>AerialToggle<cr>", { desc = "Aerial Toggle" })
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    map('n', 'gD', vim.lsp.buf.declaration, opts)
-    map('n', 'gd', vim.lsp.buf.definition, opts)
-    map('n', 'K', vim.lsp.buf.hover, opts)
-    map('n', 'gi', vim.lsp.buf.implementation, opts)
-    map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-    map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    map('n', '<leader>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    map('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-    map('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-    map('n', 'gr', vim.lsp.buf.references, opts)
-    map('n', '<leader>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		map('n', 'gD', vim.lsp.buf.declaration, opts)
+		map('n', 'gd', vim.lsp.buf.definition, opts)
+		map('n', 'K', vim.lsp.buf.hover, opts)
+		map('n', 'gi', vim.lsp.buf.implementation, opts)
+		map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+		map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		map('n', '<leader>wl', function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		map('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+		map('n', '<leader>rn', vim.lsp.buf.rename, opts)
+		map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+		map('n', 'gr', vim.lsp.buf.references, opts)
+		map('n', '<leader>f', function()
+			vim.lsp.buf.format { async = true }
+		end, opts)
 
-    map('i', '<C-q>', vim.lsp.buf.signature_help, opts)
+		map('i', '<C-q>', vim.lsp.buf.signature_help, opts)
 
-  end,
+	end,
 })
 -- Git
 map("n", "<A-g>", function() require("neogit").open({}) end, { desc = "NeoGit Status" })
 -- LuaSnip
 local ls = require("luasnip")
-map({"i"}, "<C-k>", function() ls.expand() end, {silent = true})
-map({"i", "s"}, "<C-l>", function() ls.jump( 1) end, {silent = true})
-map({"i", "s"}, "<C-j>", function() ls.jump(-1) end, {silent = true})
-map({"i", "s"}, "<C-e>", function()
+map({ "i" }, "<C-k>", function() ls.expand() end, { silent = true })
+map({ "i", "s" }, "<C-l>", function() ls.jump(1) end, { silent = true })
+map({ "i", "s" }, "<C-j>", function() ls.jump(-1) end, { silent = true })
+map({ "i", "s" }, "<C-e>", function()
 	if ls.choice_active() then
 		ls.change_choice(1)
 	end
-end, {silent = true})
+end, { silent = true })
 
 
 -- Legendary
--- editor/lua/custom/options.lua
+++ editor/lua/custom/options.lua
@@ -1,41 +1,41 @@
--- Set options
-vim.opt.backspace="indent,eol,nostop" -- Don't stop backspace at i
-vim.opt.clipboard="unnamedplus" -- Connection to the system clipboard
-vim.opt.completeopt="menuone,noselect" -- Options for insert mode comple
-vim.opt.copyindent=true -- Copy the previous indentation on autoindenting
-vim.opt.cursorline=true -- Highlight the text line of the cursor
-vim.opt.expandtab=true -- Enable the use of space in tab
---vim.opt.fileencoding="utf-8" -- File content encoding for the buffer
---vim.opt.fillchars+="eob:' '" -- Disable `~` on nonexistent lines
-vim.opt.history=100 -- Number of commands to remember in a history table
-vim.opt.ignorecase=true -- Case insensitive searching
-vim.opt.laststatus=3 -- globalstatus
-vim.opt.lazyredraw=true -- lazily redraw screen
-vim.opt.mouse="a" -- Enable mouse support
-vim.opt.number=true -- Show numberline
-vim.opt.preserveindent=true -- Preserve indent structure as much as possible
-vim.opt.pumheight=10 -- Height of the pop up menu
-vim.opt.relativenumber=true -- Show relative numberline
-vim.opt.scrolloff=8 -- Number of lines to keep above and below the cursor
-vim.opt.shiftwidth=2 -- Number of space inserted for indentation
-vim.opt.showmode=false -- Disable showing modes in command line
-vim.opt.sidescrolloff=8 -- Number of columns to keep at the sides of the cursor
-vim.opt.signcolumn="yes" -- Always show the sign column
-vim.opt.smartcase=true -- Case sensitivie searching
-vim.opt.splitbelow=true -- Splitting a new window below the current one
-vim.opt.splitright=true -- Splitting a new window at the right of the current o
-vim.opt.swapfile=false -- Disable use of swapfile for the buffer
-vim.opt.tabstop=2 -- Number of space in a tab
-vim.opt.termguicolors=true -- Enable 24-bit RGB color in the TUI
-vim.opt.timeoutlen=300 -- Length of time to wait for a mapped sequence
-vim.opt.undofile=true -- Enable persistent undo
-vim.opt.updatetime=300 -- Length of time to wait before triggering the plugin
-vim.opt.wrap=false -- Disable wrapping of lines longer than the width of window
-vim.opt.writebackup=false -- Disable making a backup before overwriting a file
-vim.opt.list=true
-
 -- [[ Setting options ]]
 -- See `:help vim.o`
+
+vim.o.backspace="indent,eol,nostop" -- Don't stop backspace at i
+vim.o.clipboard="unnamedplus" -- Connection to the system clipboard
+vim.o.completeopt="menuone,noselect" -- Options for insert mode comple
+vim.o.copyindent=true -- Copy the previous indentation on autoindenting
+vim.o.cursorline=true -- Highlight the text line of the cursor
+vim.o.expandtab=true -- Enable the use of space in tab
+--vim.o.fileencoding="utf-8" -- File content encoding for the buffer
+--vim.o.fillchars+="eob:' '" -- Disable `~` on nonexistent lines
+vim.o.history=100 -- Number of commands to remember in a history table
+vim.o.ignorecase=true -- Case insensitive searching
+vim.o.laststatus=3 -- globalstatus
+vim.o.lazyredraw=true -- lazily redraw screen
+vim.o.mouse="a" -- Enable mouse support
+vim.o.number=true -- Show numberline
+vim.o.preserveindent=true -- Preserve indent structure as much as possible
+vim.o.pumheight=10 -- Height of the pop up menu
+vim.o.relativenumber=true -- Show relative numberline
+vim.o.scrolloff=8 -- Number of lines to keep above and below the cursor
+vim.o.shiftwidth=2 -- Number of space inserted for indentation
+vim.o.showmode=false -- Disable showing modes in command line
+vim.o.sidescrolloff=8 -- Number of columns to keep at the sides of the cursor
+vim.o.signcolumn="yes" -- Always show the sign column
+vim.o.smartcase=true -- Case sensitivie searching
+vim.o.splitbelow=true -- Splitting a new window below the current one
+vim.o.splitright=true -- Splitting a new window at the right of the current o
+vim.o.swapfile=false -- Disable use of swapfile for the buffer
+vim.o.tabstop=2 -- Number of space in a tab
+vim.o.termguicolors=true -- Enable 24-bit RGB color in the TUI
+vim.o.timeoutlen=300 -- Length of time to wait for a mapped sequence
+vim.o.undofile=true -- Enable persistent undo
+vim.o.updatetime=300 -- Length of time to wait before triggering the plugin
+vim.o.wrap=false -- Disable wrapping of lines longer than the width of window
+vim.o.writebackup=false -- Disable making a backup before overwriting a file
+vim.o.list=true
+vim.o.listchars="tab:󰌒 ,trail:∘"--,nbsp:󰯉,eol:󰯉"
 
 -- Set highlight on search
 vim.o.hlsearch = false
