845399f3b70ca3657da58941a2554830d112d95d
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2023-12-20 10:34:33 +0100

    chgdap

--- editor/lua/custom/dap.lua
+++ editor/lua/custom/dap.lua
@@ -23,6 +23,30 @@ dap.configurations.rust = {
   },
 }
 
+
+dap.adapters = {
+  python = {
+    type = "executable",
+    command = "/usr/bin/python",
+    args = { "-m", "debugpy.adapter" },
+  }
+}
+
+
+dap.configurations = {
+  python = {
+    {
+      type = "python",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      pythonPath = function()
+        return "python"
+      end,
+    },
+  },
+}
+
 -- dap.adapters.lldb = {
 --   type = 'executable',
 --   command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
