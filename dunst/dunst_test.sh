f1adeb5eb058486a9046c3ea81fcfce78d6bc53d
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2024-01-03 10:41:24 +0100

    chg

--- dunst/dunst_test.sh
+++ dunst/dunst_test.sh
@@ -1,4 +1,10 @@
 #!/bin/env bash
 
 # canberra-gtk-play -i service-login
+
+if [[ $DUNST_BODY =~ teams.microsoft.com ]]; then
+  gsound-play -i service-logout
+  exit 0
+fi
+
 gsound-play -i service-login
