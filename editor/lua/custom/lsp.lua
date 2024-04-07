7286e38ad329837de8296c26e3831eca9f26bb7d
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2024-04-07 12:38:49 +0200

    chg

--- editor/lua/custom/lsp.lua
+++ editor/lua/custom/lsp.lua
@@ -30,7 +30,7 @@ local servers = {
 			telemetry = { enable = false },
 		},
 	},
-	stylua = {},
+	-- stylua = {},
 	bashls = {},
 	intelephense = {
 
@@ -218,7 +218,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.eslint,
+		-- null_ls.builtins.diagnostics.eslint,
 		null_ls.builtins.completion.spell,
 	},
 })
