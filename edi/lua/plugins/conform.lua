96925899d46037b08c1393c83b34b20d3276fc1f
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2024-08-01 09:33:01 +0200

    chg

--- edi/lua/plugins/conform.lua
+++ edi/lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
 			require("conform").setup({
 				formatters_by_ft = {
 					lua = { "stylua" },
+          python = { "ruff" },
 				},
 				default_format_opts = {
 					lsp_format = "fallback",
