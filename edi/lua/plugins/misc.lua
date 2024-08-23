260dfce3233d0848b00f114c351ea6f2fb219a6c
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2024-08-23 09:50:00 +0200

    chg

--- edi/lua/plugins/misc.lua
+++ edi/lua/plugins/misc.lua
@@ -1,98 +1,98 @@
 return {
-	"nvim-tree/nvim-web-devicons",
-	"tpope/vim-vinegar",
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
+  "nvim-tree/nvim-web-devicons",
+  "tpope/vim-vinegar",
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  },
 
-	"anuvyklack/hydra.nvim",
-	{
-		"rcarriga/nvim-notify",
-		configure = function()
-			local notify = require("notify")
-			-- notify.setup({ stages = "slide" })
-			notify.setup()
-			vim.notify = notify
-		end,
-	},
+  "anuvyklack/hydra.nvim",
+  {
+    "rcarriga/nvim-notify",
+    configure = function()
+      local notify = require("notify")
+      -- notify.setup({ stages = "slide" })
+      notify.setup()
+      vim.notify = notify
+    end,
+  },
 
-	{
-		"numToStr/Comment.nvim",
-		dependencies = {
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
-		config = function()
-			require("Comment").setup({
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			})
+  {
+    "numToStr/Comment.nvim",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
 
-			local ft = require("Comment.ft")
-			ft.set("typescriptreact", { "{/* %s */}", "{/* %s */}" })
-		end,
-	},
+      local ft = require("Comment.ft")
+      ft.set("typescriptreact", { "{/* %s */}", "{/* %s */}" })
+    end,
+  },
 
-	{
-		"rcarriga/nvim-notify",
-		config = function()
-			local notify = require("notify")
-			notify.setup({ stages = "slide" })
-			vim.notify = notify
-		end,
-	},
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      local notify = require("notify")
+      notify.setup({ stages = "slide" })
+      vim.notify = notify
+    end,
+  },
 
-	{
-		"tpope/vim-dadbod",
-		dependencies = {
-			"kristijanhusak/vim-dadbod-completion",
-			"kristijanhusak/vim-dadbod-ui",
-		},
-	},
+  {
+    "tpope/vim-dadbod",
+    dependencies = {
+      "kristijanhusak/vim-dadbod-completion",
+      "kristijanhusak/vim-dadbod-ui",
+    },
+  },
 
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local oil = require("oil")
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local oil = require("oil")
 
-			oil.setup({
+      oil.setup({
 
-				use_default_keymaps = false,
-				keymaps = {
-					["g?"] = "actions.show_help",
-					["<CR>"] = "actions.select",
-					["<C-s>"] = {
-						"actions.select",
-						opts = { vertical = true },
-						desc = "Open the entry in a vertical split",
-					},
-					["<C-x>"] = {
-						"actions.select",
-						opts = { horizontal = true },
-						desc = "Open the entry in a horizontal split",
-					},
-					["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
-					["<C-p>"] = "actions.preview",
-					["<C-c>"] = "actions.close",
-					["<C-r>"] = "actions.refresh",
-					["-"] = "actions.parent",
-					["H"] = "actions.parent",
-					["L"] = "actions.select",
-					["_"] = "actions.open_cwd",
-					["`"] = "actions.cd",
-					["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
-					["gs"] = "actions.change_sort",
-					["gx"] = "actions.open_external",
-					["g."] = "actions.toggle_hidden",
-					["g\\"] = "actions.toggle_trash",
-				},
-			})
-			vim.keymap.set("n", "<Bslash>b", ":Oil<CR>", { desc = "Oil file browser" })
-		end,
-	},
+        use_default_keymaps = false,
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-s>"] = {
+            "actions.select",
+            opts = { vertical = true },
+            desc = "Open the entry in a vertical split",
+          },
+          ["<C-x>"] = {
+            "actions.select",
+            opts = { horizontal = true },
+            desc = "Open the entry in a horizontal split",
+          },
+          ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+          ["<C-p>"] = "actions.preview",
+          ["<C-c>"] = "actions.close",
+          ["<C-r>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["H"] = "actions.parent",
+          ["L"] = "actions.select",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+          ["gs"] = "actions.change_sort",
+          ["gx"] = "actions.open_external",
+          ["g."] = "actions.toggle_hidden",
+          ["g\\"] = "actions.toggle_trash",
+        },
+      })
+      vim.keymap.set("n", "<Bslash>b", ":Oil<CR>", { desc = "Oil file browser" })
+    end,
+  },
 }
