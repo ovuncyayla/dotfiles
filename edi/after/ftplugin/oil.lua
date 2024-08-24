1f69ad5b95968358adba3e86672635e251a89773
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2024-08-24 13:29:51 +0200

    chg

--- edi/after/ftplugin/oil.lua
+++ edi/after/ftplugin/oil.lua
@@ -1,4 +1,5 @@
 local oil = require('oil')
+local fzf = require('fzf-lua')
 
 local function oil_copy()
 	local entry = oil.get_cursor_entry()
@@ -8,4 +9,16 @@ local function oil_copy()
 	vim.fn.setreg("+", entry_path)
 end
 
+local function oil_files()
+	local cur_dir = oil.get_current_dir()
+  fzf.files({ cwd = cur_dir })
+end
+
+local function oil_grep()
+	local cur_dir = oil.get_current_dir()
+  fzf.live_grep({ cwd = cur_dir })
+end
+
 vim.keymap.set("n", "<leader>y", oil_copy, { desc = "Oil copy path", buffer = true })
+vim.keymap.set("n", "f.", oil_files, { desc = "Oil files", buffer = true })
+vim.keymap.set("n", "g.", oil_grep, { desc = "Oil files", buffer = true })
--- edi/lua/plugins/misc.lua
+++ edi/lua/plugins/misc.lua
@@ -88,7 +88,7 @@ return {
           ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
           ["gs"] = "actions.change_sort",
           ["gx"] = "actions.open_external",
-          ["g."] = "actions.toggle_hidden",
+          ["gh"] = "actions.toggle_hidden",
           ["g\\"] = "actions.toggle_trash",
         },
       })
