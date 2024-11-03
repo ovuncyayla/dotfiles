ca2603b816a56d3b5ce74365339b84ee0c5ddcfd
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2024-11-03 06:18:13 +0100

    chg

--- dunst/dunst_test.sh
+++ dunst/dunst_test.sh
@@ -3,7 +3,7 @@
 # canberra-gtk-play -i service-login
 
 if [[ $DUNST_BODY =~ teams.microsoft.com ]]; then
-  gsound-play -f ~/dotfiles/dunst/coin.wav
+  aplay -f ~/dotfiles/dunst/coin.wav
   exit 0
 fi
-gsound-play -f ~/dotfiles/dunst/notificaton1.wav
+aplay -f ~/dotfiles/dunst/notificaton1.wav
