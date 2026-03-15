57e56cbe759f36c013cf0457b4ca3446130e5ff4
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2026-03-15 23:19:35 +0100

    chg

---
+++ nvim-lazy/lua/plugins/ui-extras.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "tpope/vim-dadbod",
+    dependencies = {
+      "kristijanhusak/vim-dadbod-completion",
+      "kristijanhusak/vim-dadbod-ui",
+    },
+  },
+
+  "mustache/vim-mustache-handlebars",
+  "Joorem/vim-haproxy",
+}
